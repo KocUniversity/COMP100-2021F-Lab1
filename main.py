4a44b0d37f68cc4133be99f729194654c3b9f5ca
tanjary21 <79201814+tanjary21@users.noreply.github.com>
2021-10-08 06:55:23 +0000

    quiz

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-import random
-p1 = random.randint(0,10)
-p2 = random.randint(0,10)
-score_p1 = 0
-score_p2 = 0
+p1 = input("Select Rock(R), Paper(P) or Scissors(S) for player 1")
+p2 = input("Select Rock(R), Paper(P) or Scissors(S) for player 2")
 
-print('hello')
+if p1 == "R":
+    if p2 == "R":
+        print("tie")
+    elif p2 == "P":
+        print("p2 won")
+    elif p2 == "S":
+        print("p1 won")
+    else:
+        print("invalid input")
+elif p1 == "P":
+    if p2 == "R":
+        print("p1 won")
+    elif p2 == "P":
+        print("tie")
+    elif p2 == "S":
+        print("p2 won")
+    else:
+        print("invalid input")
+elif p1 == "S":
+    if p2 == "R":
+        print("p1 won")
+    elif p2 == "P":
+        print("p2 won")
+    elif p2 == "S":
+        print("tie")
+    else:
+        print("invalid input")
+else:
+    print("invalid input")
